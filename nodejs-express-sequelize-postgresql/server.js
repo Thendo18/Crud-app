84bd46982ddc388a5e1af3bb150c56d31eba2c6b
Thendo18 <thendomakherana@gmail.com>
2022-06-06 01:00:48 +0200

    changes

--- nodejs-express-sequelize-postgresql/server.js
+++ nodejs-express-sequelize-postgresql/server.js
@@ -11,6 +11,9 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // simple route
+
+
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to thendo's application." });
 });
@@ -19,3 +22,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
+
+
