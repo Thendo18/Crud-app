1161ec7cc4c34cf769f3c9f86947c011848fc871
Thendo18 <thendomakherana@gmail.com>
2022-06-06 02:25:25 +0200

    Set up App Module

--- Angular10Crud/src/app/services/tutorial.service.ts
+++ Angular10Crud/src/app/services/tutorial.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+const baseUrl = 'http://localhost:8080/api/tutorials';
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getAll(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+  get(id: any): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+  create(data: { title: string; description: string; }): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+  update(id: any, data: { title: any; description: any; published: any; } | null): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+  findByTitle(title: string): Observable<any> {
+    return this.http.get(`${baseUrl}?title=${title}`);
+  }
 }
